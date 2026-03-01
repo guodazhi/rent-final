6c32dd0a648ff09b5a49718abe05ce74aa0605ae
guodazhi <guodazhi569@gmail.com>
2026-03-02 00:09:52 +1000

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,8 @@
 // assets/config.js
 window.APP_CONFIG = {
+  // ✅ 注意：这里必须是你正确的 Project ID：skfipmpumnkgiltzzieb
   SUPABASE_URL: "https://skfipmpumnkgiltzzieb.supabase.co",
+
+  // ✅ 这里粘贴你自己的 anon key（以 sb_publishable_ 开头那串）
   SUPABASE_ANON_KEY: "sb_publishable_bPYfdckQj2hEeT4A8Ff2TQ__wDVB4r8"
 };
